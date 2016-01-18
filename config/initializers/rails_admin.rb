46654ee49517213f0802cdec352811412cfda789
Kevin Yang <KevinYang@rescomp-15-313008.stanford.edu>
2016-01-17 23:06:24 -0800

    edited admin panel list values

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,34 +1,54 @@
 RailsAdmin.config do |config|
 
 
-   config.authenticate_with do
+  #Check if the user is signed in
+  config.authenticate_with do
     unless current_user
       session[:return_to] = request.url
       redirect_to '/', alert:"You must first log in or sign up before accessing this page."
     end
   end 
 
+  #If the current_user.admin field is false than redirect
   config.authorize_with do
     redirect_to '/', alert: "You are not authorized to access that page" unless current_user.admin == true
   end
-  ### Popular gems integration
+  
+  #Choose what to show in the list for Lab
+  config.model 'Lab' do
+    list do
+      field :id
+      field :title
+      field :email
+      field :user_id 
+      field :director
+      field :webpage
+      field :created_at
+      field :updated_at 
+      field :description
+      field :req
+      field :suggested
+      field :year
+    end
+  end
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  #Choose what to show in the list for User
+  config.model 'User' do
+    list do
+      field :id
+      field :email
+      field :labs
+      field :current_sign_in_at
+      field :last_sign_in_at
+      field :admin
+      field :created_at
+      field :current_sign_in_ip
+      field :last_sign_in_ip
+      field :created_at
+      field :updated_at
+      field :reset_password_sent_at
+    end
+  end
 
   config.actions do
     dashboard                     # mandatory
